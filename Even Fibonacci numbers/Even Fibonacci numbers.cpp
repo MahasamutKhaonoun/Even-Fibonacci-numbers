272612296733dcdd5af74f3b5bb97f52ee442300
เอกภพ ขาวนวล < @DESKTOP-SURU41J>
2020-08-14 15:33:27 +0700

    Add project files.

---
+++ Even Fibonacci numbers/Even Fibonacci numbers.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() 
+{
+	int a, b, c, sum;
+	a = b = c = 1;
+	sum = 0;
+	for (;c <= 4000000;) 
+	{
+		if (c % 2 == 0)
+		{
+			sum += c;
+		}
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	printf("%d ", sum);
+
+}
